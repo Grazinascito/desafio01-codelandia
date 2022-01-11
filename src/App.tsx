01c67d2dc1b4bd036f775c7fe16dd6a519add5d9
GraziNascito <grazi.social84@gmail.com>
2022-01-10 22:17:46 -0300

    adicionando funcionalidade de trocar status do coração

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { MainContent } from "./components/MainContent";
 import "./style/global.scss";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      date: "02 de jul, 2021",
+      heart: false,
+      title: "Agora é oficial: o Windows 11 está vindo",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.",
+    },
+    {
+      id: 2,
+      date: "02 de jul, 2021",
+      heart: false,
+      title: "Tim Berners-Lee vai leiloar código-fonte da web",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.",
+    },
+    {
+      id: 3,
+      date: "02 de jul, 2021",
+      heart: false,
+      title: "Tem Firefox novo no pedaço e você vai querer migrar",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.",
+    },
+    {
+      id: 4,
+      date: "02 de jul, 2021",
+      heart: false,
+      title: "John McAfee, criador do antivírus McAfee, morre",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.",
+    },
+  ];
+
+  interface DataProps {
+    id: number;
+    date: string;
+    heart: boolean;
+    title: string;
+    text: string;
+    
+  }
+
+  const [initData, setInitData] = useState<DataProps[]>([]);
+
+
+  useEffect(() => {
+    setInitData(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <Header />
 
       <div className="main-container">
-        <MainContent />
+        <MainContent
+          initData={initData}
+        
+          setInitData={setInitData}
+        />
       </div>
     </div>
   );
--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,33 +1,60 @@
 import "./style.scss";
 import heartIconWhite from "../../assets/HeartIconWhite.svg";
+import heartIcon from "../../assets/heartIcon.svg";
 
-export function MainContent() {
+type InitDataProps = {
+  id: number;
+  date: string;
+  heart: boolean;
+  title: string;
+  text: string;
+};
+
+type MainContentProps = {
+  initData: InitDataProps[];
+  setInitData: (val: InitDataProps[]) => void | boolean;
+};
+
+export function MainContent({
+  initData,
+
+  setInitData,
+}: MainContentProps) {
+  function handleChangeHeart(id: number) {
+    
+    const findData = initData.map((data) => {
+      if (data.id === id) {
+        data.heart = !data.heart;
+      }
+      return data
+    });
+
+    setInitData(findData)
+  }
+
   return (
     <main>
-      <div className="main-card">
-        <div className="main-header">
-          <span>02 de jul, 2021</span>
-          <span>
-            <img src={heartIconWhite} alt="heartIconWhite" />
-          </span>
-        </div>
-        <div className="main-text">
-          <h1>Agora é oficial: o Windows 11 está vindo</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.</p>
-        </div>
-      </div>
-      <div className="main-card">
-        <div className="main-header">
-          <span>02 de jul, 2021</span>
-          <span>
-            <img src={heartIconWhite} alt="heartIconWhite" />
-          </span>
-        </div>
-        <div className="main-text">
-          <h1>Agora é oficial: o Windows 11 está vindo</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.</p>
-        </div>
-      </div>
+      {initData.length > 0 &&
+        initData.map((data) => {
+          return (
+            <div className="main-card" key={data.id}>
+              <div className="main-header">
+                <span>{data.date}</span>
+                <span>
+                  <img
+                    src={data.heart ? heartIcon : heartIconWhite}
+                    alt="heartIconWhite"
+                    onClick={() => handleChangeHeart(data.id)}
+                  />
+                </span>
+              </div>
+              <div className="main-text">
+                <h1>{data.title}</h1>
+                <p>{data.text}</p>
+              </div>
+            </div>
+          );
+        })}
     </main>
   );
 }
